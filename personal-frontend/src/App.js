357087dbf24cf912033ce0d33a8a5a333725970f
MichalRajzer <michal.rajzer03@gmail.com>
2022-07-04 19:45:49 +0200

    ensured responsiveness

--- personal-frontend/src/App.js
+++ personal-frontend/src/App.js
@@ -1,15 +1,53 @@
-import { Router } from "@reach/router";
+import { Router, Link } from "@reach/router";
 
 import Posts from "./components/posts";
 import Post from "./components/post";
 
 function App() {
   return (
-    <Router>
-      <Posts path="/" />
-      <Post path="/posts/:id" />
-    </Router>
+    <div>
+      <Navbar current="/">About</Navbar>
+      <Router>
+        <About path="/" />
+        <Posts path="/blog" />
+        <Post path="/blog/posts/:id" />
+      </Router>
+    </div>
   );
 }
+
+const About = (props) => (
+  <div className="content">
+    <div className="nameHeader">
+      <div>
+        <h1>Hey!</h1>
+        <h1>My name is Michał</h1>
+        <h1> I'm a fullstack developer!</h1>
+      </div>
+    </div>
+  </div>
+);
+
+const Navbar = (props) => (
+  <div className="navigation">
+    <nav className="navbar">
+      <Link to={props.current} className="nav-link">
+        Michał.Rajzer({props.children})
+      </Link>
+      <Link to="/#" className="nav-link">
+        About
+      </Link>
+      <Link to="/#" className="nav-link">
+        Projects
+      </Link>
+      <Link to="/blog" className="nav-link">
+        Blog
+      </Link>
+      <Link to="/#" className="nav-link">
+        Contact
+      </Link>
+    </nav>
+  </div>
+);
 
 export default App;
