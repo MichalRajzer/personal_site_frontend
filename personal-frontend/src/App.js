4f671d361b6ca213610ea0c506926411742017d3
MichalRajzer <michal.rajzer03@gmail.com>
2022-07-19 13:10:14 +0200

    QC bcuz I need to get on another pc

--- personal-frontend/src/App.js
+++ personal-frontend/src/App.js
@@ -20,20 +20,32 @@ function App() {
 
 const About = (props) => (
   <div>
+    <div className="contentBlockOne">
     <div className="nameHeader">
-      <div style={{ padding: 0, margin: 0 }}>
+      <div style={{ padding: 0, margin: 0}}>
         <h1>Hey!</h1>
         <h1>My name is Michał</h1>
-        <h1> I'm a fullstack developer!</h1>
+        <h1>I'm a fullstack developer!</h1>
       </div>
     </div>
-    <svg class="arrows">
-      <path class="a1" d="M0 0 L30 32 L60 0"></path>
-      <path class="a2" d="M0 20 L30 52 L60 20"></path>
-      <path class="a3" d="M0 40 L30 72 L60 40"></path>
+    <svg className="arrows">
+      <path className="a1" d="M0 0 L30 32 L60 0"></path>
+      <path className="a2" d="M0 20 L30 52 L60 20"></path>
+      <path className="a3" d="M0 40 L30 72 L60 40"></path>
     </svg>
-
-    <div></div>
+    <div className="spacer spacerOne"></div>
+    </div>
+    <div className="textBlock textOne">
+      <h2 className="headTwo">Something more about me.</h2>
+      <p>
+        I attended highschool from 2019 to 2022 and during that time I studied
+        in the International Baccalaureate programme. I took up higher level
+        computer science, maths, and physics. I started my journey with
+        programming when I was 12 and my father introduced me to the Arduino
+        ecosystem. Nowadays I focus mostly on webdev but I dabble in IoT and
+        automation.
+      </p>
+    </div>
   </div>
 );
 
